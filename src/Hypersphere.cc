ed15e7acd3219af85e44a1903975a57f4789bf6c
TomasZlamal <tomaszlamal.github@gmail.com>
2025-09-04 10:59:51 +0200

    added todos to hypersphere collision logic

--- src/Hypersphere.cc
+++ src/Hypersphere.cc
@@ -10,13 +10,13 @@ int Hypersphere::vec4Collides(vec4 vec) {
   c = -(m_r * m_r) + m_center.x * m_center.x + m_center.y * m_center.y +
       m_center.z * m_center.z + m_center.w * m_center.w;
   if ((b * b - 4 * a * c) > 0) {
-
+    // check if t1, t2 are negative. Stop returning negative values, since that would mean going behing the camera.
+    // then return the value closest to the camera, so it should actually be std::min, not std::max.
     float D = (b * b - 4 * a * c);
     float t1 = (-b + std::sqrt(D)) / (2 * a);
     float t2 = (-b - std::sqrt(D)) / (2 * a);
     return std::max(t1, t2);
-  } else if ((b * b - 4 * a * c) == 0)
-    return 0;
+  }
   return 0;
 }
 } // namespace rt4
